ed42a049d56c9492abf4c13a2c790668a94979d4
AdityaGavali <adityagavali18@gmail.com>
2023-11-21 16:37:09 +0530

    ok

--- ass4.cpp
+++ ass4.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 // Function to calculate (base^exponent) % modulo efficiently
 int mod_pow(int base, int exponent, int modulo) {
@@ -17,45 +19,47 @@ int main() {
     // Constants (publicly known)
     int p = 23;  // Prime modulus
     int g = 5;   // Primitive root modulo p
-
+  // or p = 7 , q = 3
     // Alice's private key
     int private_key_alice;
-    std::cout << "Alice, enter your private key: ";
-    std::cin >> private_key_alice;
+    cout << "Alice, enter your private key: ";
+    cin >> private_key_alice;
 
     // Bob's private key
     int private_key_bob;
-    std::cout << "Bob, enter your private key: ";
-    std::cin >> private_key_bob;
+    cout << "Bob, enter your private key: ";
+    cin >> private_key_bob;
+    
+    // Eve's private key
+    int private_key_eve;
+    cout << "Eve, enter your private key: ";
+    cin >> private_key_eve;
 
     // Alice computes her public key
     int public_key_alice = mod_pow(g, private_key_alice, p);
 
     // Bob computes his public key
     int public_key_bob = mod_pow(g, private_key_bob, p);
-
-    // MITM attack (Eve)
-    int intercepted_public_key_alice = public_key_alice;
-    int intercepted_public_key_bob = public_key_bob;
-
-    // Attacker (Eve) replaces public keys with her own
-    public_key_alice = intercepted_public_key_bob;
-    public_key_bob = intercepted_public_key_alice;
+    
+    // Eve computes his public key
+    int public_key_eve = mod_pow(g, private_key_eve, p);
 
     // Shared secret calculation
     int shared_secret_alice = mod_pow(public_key_bob, private_key_alice, p);
     int shared_secret_bob = mod_pow(public_key_alice, private_key_bob, p);
+    
+    // MITM attack
+    // int shared_secret_alice = mod_pow(public_key_eve, private_key_alice, p);
+    // int shared_secret_bob = mod_pow(public_key_eve, private_key_bob, p);
 
     // Display shared secrets
-    std::cout << "Shared secret computed by Alice: " << shared_secret_alice << std::endl;
-    std::cout << "Shared secret computed by Bob: " << shared_secret_bob << std::endl;
+    cout << "Shared secret computed by Alice: " << shared_secret_alice << endl;
+    cout << "Shared secret computed by Bob: " << shared_secret_bob << endl;
 
     if (shared_secret_alice == shared_secret_bob) {
-        std::cout << "Communication is secure. Messages are not compromised." << std::endl;
+        cout << "Communication is secure. Messages are not compromised." << endl;
     } else {
-        std::cout << "MITM attack successful! Eve has intercepted the messages." << std::endl;
-        std::cout << "Eve's intercepted data: " << shared_secret_alice << std::endl;
+        cout << "MITM attack successful! Eve has intercepted the messages." << endl;
+        cout << "Eve's intercepted data: " << shared_secret_alice << endl;
     }
-
-    return 0;
 }
